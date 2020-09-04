fe3dea3b420402855e167a391680ab04d941c4ca
remboyd <37636497+remboyd@users.noreply.github.com>
2020-09-04 04:26:39 +0300

    add main directories

---
+++ src/com/lr2/Move.java
@@ -0,0 +1,11 @@
+package com.lr2;
+
+public class Move {
+    public int index;
+    public int score;
+
+    public Move(int index, int score) {
+        this.index = index;
+        this.score = score;
+    }
+}
